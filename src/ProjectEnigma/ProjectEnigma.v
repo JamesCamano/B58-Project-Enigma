5e97f1123ce8692124f5f052b02e290282329365
Jesse Niranjan Francispillai <franc272@IITS-I402-13.utsc-labs.utoronto.ca>
2019-03-12 20:57:19 -0400

    more bug fixes + import - fixed bugs with rotor component - added hex display module

--- src/ProjectEnigma/ProjectEnigma.v
+++ src/ProjectEnigma/ProjectEnigma.v
@@ -1,17 +1,67 @@
 `timescale 1ns / 1ns
 
+/* 
+NOTE: KEY SYNTAX
+
+input [n:m] KEY
+...
+KEY[j]
+
+
+HEX SYNTAX
+
+output [6:0]HEXN
+
+...
+
+... = HEXN
+
+*/
+
 module main(
-		output [17:0] LEDR,
-		input [17:0]SW
+		output [7:0] LEDG,
+		output [6:0] HEX0,
+		output [6:0] HEX2,
+		output [6:0] HEX3,
+		input [17:0]SW,
+		input [0:3] KEY,
+		input CLOCK_50
 		);
 
-
+	localparam ONE = 1'b1;
+	wire key_pressed = 1'b1 - KEY[2]; // not(key[2])
+	wire [3:0] state;
+	wire [7:0] rotor_out;
+	
 	// rotor
 	rotor_0_25 rotor(
-		.rotor_out(LEDR[6:0]),
-		.clk(SW[17]),
+		.rotor_out(rotor_out),
+		.TEMP_STATE(state),
+		.clk(CLOCK_50),
+		.user_increment(KEY[2]),
 		.load_init_state(SW[16]),
 		.rotor_init_state(SW[4:0])
 	);
+	
+	assign LEDG[7] = key_pressed;
+	assign LEDG[6:0] = rotor_out;
+	
+	// TEMP - seeing state
+	hex_display state_display(
+		.IN(state),
+		.OUT(HEX0)
+	);
+	
+	// TEMP: seeing rotor value
+	hex_display rotor_val_low(
+		.IN(rotor_out[3:0]),
+		.OUT(HEX2)
+	);
+	
+	hex_display rotor_val_high(
+		.IN({1'b0, rotor_out[6:4]}),
+		.OUT(HEX3)
+	);
+	
 	
 endmodule
--- src/enigma/rotor.v
+++ src/enigma/rotor.v
@@ -21,55 +21,64 @@
 */
 module rotor_0_25(
   output reg [6:0] rotor_out,   // 7-bit representation. note -always non-negative
+  output reg [3:0] TEMP_STATE,   // a temporary output to see state
   input clk,                    // sync incrementer
+  input user_increment,
   input load_init_state,        // async set
   input [4:0] rotor_init_state
   );
-  
+  // 25 = 16 + 8 + 
   localparam TWO_BIT_SIGN_EXT = 2'b00;
   localparam DEFAULT_VALUE = 7'd0;
-  localparam BEGINNING_VALUE = 7'd0, END_VALUE = 7'd25;
-  localparam ONE = 7'd1;
+  localparam BEGINNING_VALUE = 7'd0, END_VALUE = 7'b0011001;
+  localparam ONE = 7'b0_000_001;
+  localparam ON = 1'b1;
 
   /* Starting, rotor does not have a value - default to zero.
 	  If the rotor has a value, then increment
 	  If the user wants to set the value, check if bad value, and 'assign' accordingly.
   */
-	
+
+  
   // STATES
   localparam NULL_VAL = 2'd0, VAL = 2'd1, USER_SET = 2'd2;
+
 	
   // STATE REGISTER
   reg [1:0] current_state;
   reg [6:0] untrimmed_rotor_value;
   
+  reg [25:0] rate_divider;
+
   always@(*)
   begin: state_table
 		case(current_state)
-			NULL_VAL: current_state = VAL; // cannot escape null value state, if have null value?
-			VAL: current_state = VAL;				// assume always in a valid valued state
-			USER_SET: current_state = VAL; 		// handle invalid value, and then go to valid value state
+			NULL_VAL: current_state = VAL; 												// cannot escape null value state, if have null value?
+			VAL: current_state = load_init_state ? USER_SET: VAL;					// assume always in a valid valued state
+			USER_SET: current_state = load_init_state ? USER_SET : VAL; 		// handle invalid value, and then go to valid value state
 		default: current_state = NULL_VAL;
 		endcase
   end // state_table
   
-  always@(*) begin: big_begin
+  always@(posedge clk) begin: big_begin // always on clock tick
 	  case(current_state)
 			NULL_VAL: 
 				begin: initialize_rotors
 					rotor_out <= DEFAULT_VALUE;
-					untrimmed_rotor_value <= DEFAULT_VALUE;
+					rate_divider <= DEFAULT_VALUE;
 				end // initialize_rotors 
 			
 			VAL: 
 				begin: increment
-					if (clk == 1'b0) 
-						begin: overflow
-							if (rotor_out == END_VALUE)
+					rate_divider <= rate_divider + ONE;									// increment rate divider-how about delay when pressing?
+					if (rate_divider == 25'd0) begin: and_condition
+						if (user_increment == ON) begin: overflow 					// WHAT
+							if (rotor_out >= END_VALUE)
 								rotor_out <= DEFAULT_VALUE;
 							else
 								rotor_out <= rotor_out + ONE;
 						end // overflow
+					end  // and_condition
 				end // increment
 				
 			USER_SET:
@@ -77,7 +86,7 @@ module rotor_0_25(
 					if (rotor_init_state > END_VALUE)
 						rotor_out <= DEFAULT_VALUE;
 					else
-						rotor_out <= rotor_init_state;
+						rotor_out <= {TWO_BIT_SIGN_EXT, rotor_init_state}; // buffer user's input
 				end // user_load
 			
 			default: 
@@ -85,6 +94,8 @@ module rotor_0_25(
 					rotor_out <= rotor_out; // don't do anything
 				end // constant
 	  endcase
+	  
+	  TEMP_STATE = {TWO_BIT_SIGN_EXT, current_state};
   end // big_begin
   
 endmodule
