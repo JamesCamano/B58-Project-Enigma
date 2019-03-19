ea6dc4f0ce2993409cf3c1f681b4a9d5dff2cb35
James Camano <jamescamano0@gmail.com>
2019-03-18 20:40:13 -0400

    Enigma - Rotor Improvements  - Started Rotor modularity by creating control circuit for rotor.

--- src/enigma/rotor.v
+++ src/enigma/rotor.v
@@ -19,7 +19,7 @@
   The incrementation of the rotor is
   based on clk.
 */
-module rotor_0_25(
+module rotor_0_25( // split to debug
   output reg [6:0] rotor_out,   // 7-bit representation. note -always non-negative
   output reg [3:0] TEMP_STATE,   // a temporary output to see state
   input clk,                    // sync incrementer
@@ -27,7 +27,7 @@ module rotor_0_25(
   input load_init_state,        // async set
   input [4:0] rotor_init_state
   );
-  // 25 = 16 + 8 + 
+  // 25 = 16 + 8 +
   localparam TWO_BIT_SIGN_EXT = 2'b00;
   localparam DEFAULT_VALUE = 7'd0;
   localparam BEGINNING_VALUE = 7'd0, END_VALUE = 7'b0011001;
@@ -39,15 +39,15 @@ module rotor_0_25(
 	  If the user wants to set the value, check if bad value, and 'assign' accordingly.
   */
 
-  
+
   // STATES
   localparam NULL_VAL = 2'd0, VAL = 2'd1, USER_SET = 2'd2;
 
-	
+
   // STATE REGISTER
   reg [1:0] current_state;
   reg [6:0] untrimmed_rotor_value;
-  
+
   reg [25:0] rate_divider;
 
   always@(*)
@@ -59,16 +59,16 @@ module rotor_0_25(
 		default: current_state = NULL_VAL;
 		endcase
   end // state_table
-  
+
   always@(posedge clk) begin: big_begin // always on clock tick
 	  case(current_state)
-			NULL_VAL: 
+			NULL_VAL:
 				begin: initialize_rotors
 					rotor_out <= DEFAULT_VALUE;
 					rate_divider <= DEFAULT_VALUE;
-				end // initialize_rotors 
-			
-			VAL: 
+				end // initialize_rotors
+
+			VAL:
 				begin: increment
 					rate_divider <= rate_divider + ONE;									// increment rate divider-how about delay when pressing?
 					if (rate_divider == 25'd0) begin: and_condition
@@ -80,7 +80,7 @@ module rotor_0_25(
 						end // overflow
 					end  // and_condition
 				end // increment
-				
+
 			USER_SET:
 				begin: user_load
 					if (rotor_init_state > END_VALUE)
@@ -88,14 +88,78 @@ module rotor_0_25(
 					else
 						rotor_out <= {TWO_BIT_SIGN_EXT, rotor_init_state}; // buffer user's input
 				end // user_load
-			
-			default: 
+
+			default:
 				begin: constant
 					rotor_out <= rotor_out; // don't do anything
 				end // constant
 	  endcase
-	  
+
 	  TEMP_STATE = {TWO_BIT_SIGN_EXT, current_state};
   end // big_begin
-  
+
+endmodule
+
+/* The control module for the rotor circuit.
+   The FSM for this cicruit changes on the positive edge of the keypress.
+
+   The control circuit is sensitive to:
+    - key(board) presses, and
+    - an active reset switch.
+
+   This control circuit has outputs:
+    - increment: tells the datapath to increment the current register value.
+                 this value will be logic-0 if the user has reset states.
+
+    - reset:     tells the datapath to reset the current state of Enigma.
+                 this bit will take precedence over increment.
+ */
+module rotor_0_25_control(
+    output reg increment,
+    output reg reset,
+    input user_increment,
+    input load_init_state);
+
+    // state register
+    reg current_state;                    // single-bit state reg
+
+    // convenience variables
+    localparam  OFF = 1'b0,
+                ON  = 1'b1;
+
+    // state table
+    localparam  ROTOR_STANDARD = 1'b0,   // when in increment mode
+                ROTOR_RESET = 1'b1;      // when user has pressed reset.
+
+    // activate whenever user has sent the user_increment flag up, or pressed
+    //  the reset button
+    // NOTE: This saves us a state, since we will implicitly be in the same state -> only input will potentially change our state.
+    always@(posedge user_increment, posedge load_init_state)
+    begin: state_table
+      case(current_state):
+        ROTOR_STANDARD:   current_state = load_init_state ? ROTOR_RESET : ROTOR_STANDARD;    // if we are in standard mode, we want to be sensitive of reset.
+        ROTOR_RESET:      current_state = load_init_state ? ROTOR_RESET : ROTOR_STANDARD;    // ''
+        default:          current_state = ROTOR_STANDARD;  // we will hit this value iff current_state has no value. Default to rotor standard, and we assume that DATAPATH will take care of the rest.
+      endcase
+    end // state_table
+
+
+    // query the current state, in the same 'line' as the above block.
+    always@(posedge user_increment, posedge load_init_state)
+    begin: enable_signals
+      // make all our output signals zero. hence we need not a default value.
+      increment = OFF;
+      reset = OFF;
+
+      case(current_state):
+        ROTOR_STANDARD: increment = ON; // reset is already off for us.
+        ROTOR_RESET:    reset = ON;     // increment is already off for us.
+      endcase
+    end // enable_signals
+
+    // by the end of this module, we should either have a 10 or 01 for <increment, reset>.
+endmodule
+
+module rotor_0_25_datapath()
+
 endmodule
