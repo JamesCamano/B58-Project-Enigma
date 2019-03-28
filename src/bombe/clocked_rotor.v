9e15320cf14ee53a6af6f37b89054ea317689ab6
James Camano <jamescamano0@gmail.com>
2019-03-28 18:07:10 -0400

    Bombe - Clocked Rotor - Added FSM and datapath for clocked rotor.

--- src/bombe/clocked_rotor.v
+++ src/bombe/clocked_rotor.v
@@ -5,7 +5,7 @@
   This rotor is positive-edge triggered.
 */
 
-/** 
+/**
 TODO: Write an FSM for this - off/on for clocked_increment and load.
 
 */
@@ -17,46 +17,89 @@ module clocked_rotor_0_25(
   input clk	                        // clock
   );
 
-  localparam SAVE_VALUE = 1'b0;
-  localparam ON = 1'b1;
-  // note that the rotor _maintains_ state when:
-  //    - clocked_increment == 0
-  //    - clocked_load == 0
-  //    - clocked_rotor_init_state = X
+  wire key_released;
+  wire reset_state;
 
-  reg [7:0] rotor_val;
-  reg clocked_increment;		// having these being the problem, rotor insensitive to clocked_increment being higher
-  reg clocked_load;
+  clocked_rotor_FSM fsm(
+    .key_release(key_released),
+    .reset_state(reset_state),
+    .clk(clk),
+    .reset(load)
+  );
 
-  // s = clk = 0 -> m = x = default
-  // s = clk = 1 -> m = y = input value
-	
-  always@(posedge clk)
-  begin: clocked_rotor
-	// set wires to default values
-	clocked_increment = 1'b0;
-	clocked_load =  1'b0;
-	
-	// the fact that the clock is positive here means that we perform whatever we need to do here
-	if (load == ON)
-		clocked_load = ON; // trigger the async load functinality
-	else if (increment == ON)
-		clocked_increment = ON; 
-		
-  end // clocked_rotor
-  
-  rotor_0_25 rotor(
+  clocked_rotor_datapath datapath(
     .rotor_out(rotor_out),
-    .user_increment(clocked_increment),
-    .load_init_state(clocked_load),
-    .rotor_init_state(rotor_init_state)	 // whether or not we switch here is predicated on clocked_load.
+    .key_release(key_release),
+    .reset_state(reset_state),
+    .rotor_init_state(rotor_init_state)
   );
 
 endmodule // clocked_rotor_0_25
 
+/*
+  The finite state machine for the clocked rotor.
+ */
+module clocked_rotor_FSM(
+  output reg key_release,             // tells the datapath to reset the increment bit
+  output reg reset_state,             // tells the datapath to reset the state of the rotor.
+  input clk,                          // clock of clocked rotor.
+  input reset);
 
-module clocked_rotor_FSM();
+  // possible states
+  localparam  SAVE_VALUE = 3'b000;
+  localparam  CHECK_VALUE = 3'b100;
+  localparam  RESET_ROTOR = 3'b001;
+
+  // convenience parameters
+  localparam  ON = 1'b1;
+  localparam  OFF = 1'b0;
+
+  reg [2:0] current_state;  // 3-bit state register
+  //reg next_state;
+
+  always @( posedge clock or reset ) begin: // state_table
+    // always go to reset if it is on, otherwise, just bounce
+    case(current_state)
+      SAVE_VALUE:       current_state = reset ? RESET_ROTOR : CHECK_VALUE;
+      CHECK_VALUE:      current_state = reset ? RESET_ROTOR : SAVE_VALUE;
+      RESET_ROTOR:      current_state = reset ? RESET_ROTOR : SAVE_VALUE;  // if we have just reset, save the value for safety
+      default:          current_state = SAVE_VALUE;                        // default to save value. Ensures that we always have a valid state.
+    endcase
+  end // state_table
+
+  // querying the current state
+  always @ ( posedge clock or reset ) begin: query_state
+    key_release = OFF;
+    reset_state = OFF;
+
+    case(current_state)
+      SAVE_VALUE:       key_release = ON;   // release the key
+      CHECK_VALUE:      key_release = OFF;  // press the key
+      RESET_ROTOR:      reset_state = ON;   // async reset.
+    endcase
+  end // query_state
 
 endmodule
 
-module clocked_rotor_datapath();
+/*
+The datapath for the clocked rotor.
+*/
+module clocked_rotor_datapath(
+  output [7:0] rotor_out;
+  input key_release;
+  input reset_state;
+  input [4:0] rotor_init_state;
+);
+
+  wire key_press = ~key_release; // if key_release is 0, then we want to increment the rotor.
+
+  // since the rotor's increment is sensitive to keypress changes, and
+  //  load_init_state is async, we should just be able to pass everything in.
+  rotor_0_25 rotor(
+    .rotor_out(rotor_out),
+    .user_increment(key_press),
+    .load_init_state(reset_state),
+    .rotor_init_state(rotor_init_state)	 // whether or not we switch here is predicated on reset_state.
+  );
+
+endmodule
