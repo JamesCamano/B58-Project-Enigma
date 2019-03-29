fddc3ef925a9f0149f9eeb014bfce67c831053c1
James Camano <jamescamano0@gmail.com>
2019-03-28 20:17:42 -0400

    Bombe FSM - State Table - added states and state table transitions for FSM.

--- src/bombe/bombe.v
+++ src/bombe/bombe.v
@@ -40,6 +40,66 @@ module bombe_control (
   input arithmetic_end                  // indicator bit for deduction success/failure.
   );
 
+  /* STATES:
+    - 1. load reg s1
+    - 2. load reg s2
+    - 3. load reg s3
+
+    - 4. deduction (start + continue) - effectively the same state
+
+    - 5. end (success/failure state)
+
+    - 6. RESET -> load reg s1
+
+    ceil(log_2(6)) = log_2(8) = 3
+  */
+
+  // STATE REGISTERS
+  reg [2:0] current_state;
+  reg [2:0] next_state;
+
+  // STATES
+  // load
+  localparam LOAD_S1        = 4'd0;
+  localparam LOAD_S1_WAIT   = 4'd1;   // intermediate state for waiting until key_press goes low. same for similarly-named states.
+
+  localparam LOAD_S2        = 4'd2;
+  localparam LOAD_S2_WAIT   = 4'd3;
+
+  localparam LOAD_S3        = 4'd4;
+  localparam LOAD_S3_WAIT   = 4'd5;
+
+  // process start / continue
+  localparam DEDUCT         = 4'd6;   // main functionality state.
+
+  // end/reset
+  localparam PROCESS_END    = 4'd7;   // success/failure state.
+  localparam RESET          = 4'd8;   //
+  localparam RESET_WAIT     = 4'd9;   // intermediate state for waiting until reset goes low.
+
+  // QUERY CURRENT STATE AND CHOOSE NEXT STATE
+  // TODO: reset stages for load.
+  always @ ( * ) begin: state_table                                         // when ANYTHING changes, check the state table.
+    case (current_state)
+        LOAD_S1:        next_state = key_press ? LOAD_S1_WAIT : LOAD_S1;    // loop in LOAD_S1 until key_press is high.
+        LOAD_S1_WAIT:   next_state = key_press ? LOAD_S1_WAIT : LOAD_S2;    // loop in waiting stage, until the key is released.
+        LOAD_S2:        next_state = key_press ? LOAD_S2_WAIT : LOAD_S2;
+        LOAD_S2_WAIT    next_state = key_press ? LOAD_S2_WAIT : LOAD_S3;
+        LOAD_S3:        next_state = key_press ? LOAD_S3_WAIT : LOAD_S3;
+        LOAD_S3_WAIT    next_state = key_press ? LOAD_S3_WAIT : DEDUCT;     // Begin deducing after this key press goes low.
+        DEDUCT          next_state = arithmetic_end ? PROCESS_END: DEDUCT;  // Until we get the arithmetic_end flag, keep on deducing.
+        PROCESS_END     next_state = reset ? RESET : PROCESS_END;           // stay in process end state until user explicitly wants to reset.
+        RESET:          next_state = RESET_WAIT;
+        RESET_WAIT:     next_state = reset ? RESET_WAIT : LOAD_S1;          // loop in reset_wait state until reset is low.
+      default: next_state = RESET;
+    endcase
+  end // state_table
+
+
+  // QUERY CURRENT STATE TO DEFINE CONTROL BITS
+
+  // SWITCH TO NEXT STATE
+
 endmodule //bombe_control
 
 module bombe_datapath (
@@ -52,5 +112,7 @@ module bombe_datapath (
   input reset,
   input rotor_enable,
   input clk);
+
+
 
 endmodule //bombe_datapath
